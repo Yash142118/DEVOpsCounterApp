dfcb61ce9bbe70612201fc4357172d8b3d1cd415
Yash Bhanushali <yashbhanushali1421@gmail.com>
2025-06-27 18:59:40 +0530

    Counter APP Createed

---
+++ src/Counter.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function Counter() {
+  const [initialValue, setInitialValue] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+    <p>Use the below buttons to Increase or Decrease the counter value.</p>
+        <div className='Parent-Counter'>
+          <div className='Counter-Adjustments'>
+            <p>INCREASE</p>
+            <button
+              className='Increase-Counter Counter-button'
+              onClick={() => setInitialValue(initialValue + 1)}
+            >
+              <p className='Icon-style'>+</p>
+            </button>
+          </div>
+
+          <div className='Counter-value Counter-Adjustments'>
+            <p>Count is</p>
+            <span>{initialValue}</span>
+          </div>
+
+          <div className='Counter-Adjustments'>
+            <p>DECREASE</p>
+            <button
+              className='Decrease-Counter Counter-button'
+              onClick={() => setInitialValue(initialValue === 0 ? initialValue : initialValue - 1)}
+            >
+              <p className='Icon-style'>-</p>
+            </button>
+          </div>
+        </div>
+        <button className='button' onClick={() => setInitialValue(0)}>Reset</button>    
+      </header>
+    </div>
+  );
+}
+
+export default Counter;
